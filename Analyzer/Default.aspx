f1fc8071c65f9e099334d8ec0e7e229490f63e37
falcon397 <michaelhuckleberry.mh@gmail.com>
2017-08-29 15:54:26 -0700

    Initial Commit

---
+++ Analyzer/Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Analyzer.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Market Analyzer</title>
+    <script src="script/javascript.js"></script>
+    <link href="style/style.css" rel="stylesheet" />
+
+    <%-- Bootstraps --%>
+    <script src="script/bootstrap.min.js"></script>
+    <link href="style/bootstrap.min.css" rel="stylesheet" />
+
+    <%-- Google Hosted Libraries --%>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="pnlUpdate">
+            <div id="pnlTable"></div>
+        </div>
+        <asp:Button ID="btnSubmit" runat="server" Text="Get Data" />
+    </form>
+</body>
+</html>
---
+++ Analyzer/script/javascript.js
@@ -0,0 +1,93 @@
+﻿/// <reference path="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" />
+$(document).ready(function () {
+
+    //Hide fields that don't show until the user does something.
+    //jQuery loads after the page, so I have to hide stuff with CSS, then with jQuery, then remove the CSS rule.
+    //There's probably a better way...
+    $('#jQueryUpdate').hide();
+    $('#lblError').hide();
+    $('#lblError').removeClass('hidden');
+
+    //Get date and set messages and input masks.
+    var d = new Date().toLocaleDateString();
+    var textEnglish = "As of " + d + ", our records reflect the following status of your application:"
+    var textSpanish = "A partir de " + d + ", nuestros archivos indican el siguiente estado de su aplicación:"
+
+    $('#lblDate').text(function () { if (SPCheck()) return textSpanish; else return textEnglish });
+    $('#txtCAPAppID').inputmask("9{7}", { placeholder: "" });
+    $('#txtLicense').inputmask("X{1,7}", {
+        placeholder: "",
+        definitions: {
+            'X': {
+                validator: "[0-9A-Za-z]",
+                cardinality: 1,
+                casing: "upper"
+            }
+        }
+    });
+});
+
+function SPCheck() {
+    return window.location.href.toLowerCase().indexOf("capstatuschecksp") >= 0 ? true : false;
+}
+
+function GetAppData() {
+    var id = $('#txtCAPAppID').val().trim();
+    var license = $('#txtLicense').val().trim();
+    if (id != '' && license != '') {
+        var data = { id: id, license: license, language: SPCheck() };
+        $.ajax({
+            type: "POST",
+            url: "CAPStatusCheck.aspx/GetAppData",
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                OnError()
+            },
+            success: function (response) {
+                OnSuccess(response)
+            }
+        });
+    }
+    else {
+        OnError();
+        return false;
+    }
+}
+
+function OnSuccess(response) {
+    if ($('#lblError').is(':visible'))
+        $('#lblError').fadeOut(400);
+
+    if ($("#jQueryUpdate").is(":visible")) {
+        $('#jQueryUpdate').slideUp(400);
+        window.setTimeout(function () {
+            setData(response);
+        }, 650);
+    }
+
+    else
+        setData(response);
+}
+
+function setData(response) {
+    var status = response.d.split(" || ");
+    $('#lblStatus').text(status[0]);
+    if (status.length > 1)
+        $('#lblDefinition').text(status[1]);
+    $('#jQueryUpdate').fadeIn("slow");
+}
+
+function OnError() {
+    if ($("#jQueryUpdate").is(":visible")) {
+        $('#jQueryUpdate').slideUp(400);
+        window.setTimeout(function () {
+            $('#lblError').fadeIn("slow");
+        }, 650);
+    }
+
+    else {
+        $('#lblError').fadeIn("slow");
+    }
+}
