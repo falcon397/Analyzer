91c20321642c4af3630f3dd83a403d4606b74165
falcon397 <michaelhuckleberry.mh@gmail.com>
2017-08-30 12:58:12 -0700

    Back to it.

--- Analyzer/Default.aspx.cs
+++ Analyzer/Default.aspx.cs
@@ -20,25 +20,33 @@ namespace Analyzer
         public static string getData()
         {
             MySqlConnectionStringBuilder conn_string = new MySqlConnectionStringBuilder();
-            conn_string.Server = "www.huckshome.com";
+            conn_string.Server = "65.78.152.229";
             conn_string.Port = 3306;
             conn_string.UserID = "analyzer";
             conn_string.Password = "EKsDhW0FPoSt8A7D";
             conn_string.Database = "analyzer";
 
-            using (MySqlConnection conn = new MySqlConnection(conn_string.ToString()))
+            using (MySqlConnection conn = new MySqlConnection("Server=huckshome.com;Database=analyzer;Uid=analyzer;Pwd=9rmXo5X0z6UCXHBW;"))
+                //conn_string.ToString()))
             using (MySqlCommand cmd = new MySqlCommand("select * from buys"))
             {
-                conn.Open();
-                using (MySqlDataReader reader = cmd.ExecuteReader())
-                {
-                    if (reader.HasRows)
+                try {
+                    conn.Open();
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
                     {
-                        while (reader.Read())
+                        if (reader.HasRows)
                         {
+                            while (reader.Read())
+                            {
 
+                            }
                         }
                     }
+                }
+
+                finally
+                {
+                    conn.Close();
                 }
             }
 
--- Analyzer/script/javascript.js
+++ Analyzer/script/javascript.js
@@ -12,7 +12,29 @@ $(document).ready(function () {
     //Disable form submit, this page solely relies on AJAX calls and JSON data, with jQuery updating the fields.
     $('#Form1').submit(function () {
         return false;
-    })
+    });
+
+    $.ajax({
+        type: "POST",
+        url: "Default.aspx/GetData",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+
+            //Mark error and log info in the console, and populate the error field.
+            console.error("Error Thrown: " + errorThrown);
+            console.error("Text Status: " + textStatus)
+            console.log(XMLHttpRequest);
+            $('#lblError').text(errorThrown);
+
+            //Process visibility or error.
+            OnError()
+        },
+        success: function (response) {
+            //Process visibility of error and charts.
+            OnSuccess(response)
+        }
+    });
 });
 
 //Get the data with AJAX call.
@@ -51,61 +73,41 @@ function toTitleCase(str)
 
 
 //For when a database and services is the primary means of retrieving data.
-//$.ajax({
-//    type: "POST",
-//    url: "Default.aspx/GetData",
-//    contentType: "application/json; charset=utf-8",
-//    dataType: "json",
-//    error: function (XMLHttpRequest, textStatus, errorThrown) {
 
-//        //Mark error and log info in the console, and populate the error field.
-//        console.error("Error Thrown: " + errorThrown);
-//        console.error("Text Status: "+ textStatus)
-//        console.log(XMLHttpRequest);
-//        $('#lblError').text(errorThrown);
-
-//        //Process visibility or error.
-//        OnError()
-//    },
-//    success: function (response) {
-//        //Process visibility of error and charts.
-//        OnSuccess(response)
-//    }
-//});
 //}
 
-//function OnSuccess(response) {
-//    if ($('#pnlError').is(':visible'))
-//        $('#pnlError').fadeOut(400);
+function OnSuccess(response) {
+    if ($('#pnlError').is(':visible'))
+        $('#pnlError').fadeOut(400);
 
-//    if ($("#pnlUpdate").is(":visible")) {
-//        $('#pnlUpdate').slideUp(400);
-//        window.setTimeout(function () {
-//            setData(response);
-//        }, 650);
-//    }
+    if ($("#pnlUpdate").is(":visible")) {
+        $('#pnlUpdate').slideUp(400);
+        window.setTimeout(function () {
+            setData(response);
+        }, 650);
+    }
 
-//    else
-//        setData(response);
-//}
+    else
+        setData(response);
+}
 
-//function setData(response) {
-//    var status = response.d.split(" || ");
-//    $('#lblStatus').text(status[0]);
-//    if (status.length > 1)
-//        $('#lblDefinition').text(status[1]);
-//    $('#pnlUpdate').fadeIn("slow");
-//}
+function setData(response) {
+    var status = response.d.split(" || ");
+    $('#lblStatus').text(status[0]);
+    if (status.length > 1)
+        $('#lblDefinition').text(status[1]);
+    $('#pnlUpdate').fadeIn("slow");
+}
 
-//function OnError(errorThrown) {
-//    if ($("#pnlError").is(":visible")) {
-//        $('#pnlError').slideUp(400);
-//        window.setTimeout(function () {
-//            $('#pnlError').fadeIn("slow");
-//        }, 650);
-//    }
+function OnError(errorThrown) {
+    if ($("#pnlError").is(":visible")) {
+        $('#pnlError').slideUp(400);
+        window.setTimeout(function () {
+            $('#pnlError').fadeIn("slow");
+        }, 650);
+    }
 
-//    else {
-//        $('#pnlError').fadeIn("slow");
-//    }
-//}
+    else {
+        $('#pnlError').fadeIn("slow");
+    }
+}
