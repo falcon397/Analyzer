069d8fb93c08bb1dda09f18e7d33ad784139ea80
falcon397 <michaelhuckleberry.mh@gmail.com>
2017-09-01 00:18:20 -0700

    Update the columns to expand dynamically and you can probably call this one done.  All that's needed is to allow more data is a drop down element to select the service and shazaam, dynamically displaying content.

--- Analyzer/script/javascript.js
+++ Analyzer/script/javascript.js
@@ -1,57 +1,24 @@
 ﻿/// <reference path="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" />
 /// <reference path="https://cdn.datatables.net/v/dt/dt-1.10.15/datatables.min.js" />
 
+//Stuff to do at page load.
 $(document).ready(function () {
-
-    //Hide fields that don't show until the user does something.
-    //jQuery loads after the page, so I have to hide stuff with CSS, then with jQuery, then remove the CSS rule.
-    //There's probably a better way...
-    $('#pnlError').hide();
-    $('#pnlError').removeClass('hidden');
-
     //Disable form submit, this page solely relies on AJAX calls and JSON data, with jQuery updating the fields.
     $('#Form1').submit(function () {
         return false;
     });
 });
 
-//Pull data from CSV file.
-function uploadCSV(event) {
-    //Get file and initialize file reader.
-    var file = event.target.files[0];
-    var reader = new FileReader();
-    var dsHead = [];
-    var dsTable = [];
-    reader.readAsBinaryString(file);
-
-    //On loading the file reader build the table.
-    reader.onload = function () {
-        var rows = reader.result.toString().split("\n");
-        rows.forEach(function sortData(row, index) {
-            var columns = row.split(",");
-            var dsTemp = [];
-            if (index < 10) {
-                for (j = 0; j < columns.length; j++)
-                    dsTemp.push({ title: "'" + cleanData(columns[j]) + "'" });
-
-                if (index > 0)
-                    dsTable.push(dsTemp);
-                else
-                    dsHead.push(dsTemp);
-            }
-        });
-        updateDataTable(dsTable, dsHead)
-    }
-}
-
-//Get the data with AJAX call.
+//Get the data with XHR call.
 function getData(service) {
     var url = 'http://huckshome.com:8080/projects/WCFNastyFans/NastyFanService.svc/' + service;
     var xhr = createCORSRequest('POST', url);
     xhr.onload = OnSuccess;
+    xhr.onerror = OnError;
     xhr.send();
 }
 
+//Build XHR object
 function createCORSRequest(method, url) {
     var xhr = new XMLHttpRequest();
     if ("withCredentials" in xhr) {
@@ -68,22 +35,9 @@ function createCORSRequest(method, url) {
     return xhr;
 }
 
-function updateDataTable(dsTable, dsHead) {
-    $('#tblTable').dataTable({
-        data: dsTable,
-        columns: dsHead
-    });
-}
-
-//Some fields have characters that don't allow for proper formatting, this is a running list of things that shouldn't be in the data.
-function cleanData(str) {
-    return str.replace(/_/g, " ").replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); });
-}
-
 //Response handler for success.
 function OnSuccess(data) {
-    var objTemp = JSON.parse(this.responseText);
-    var objJSON = JSON.parse(objTemp.GetBuysDataResult);
+    var objJSON = JSON.parse(JSON.parse(this.responseText).GetBuysDataResult);
     var dsTable = [];
     var dsHead = [];
 
@@ -100,7 +54,7 @@ function OnSuccess(data) {
             });
 
             for (i = 0; i < headData.length; i++)
-                dsHead.push({ title: headData[i].toString() });
+                dsHead.push({ title: cleanData(headData[i].toString()) });
         }
 
     });
@@ -110,4 +64,16 @@ function OnSuccess(data) {
 //Response handler for error.
 function OnError(errorThrown) {
 
+}
+
+function updateDataTable(dsTable, dsHead) {
+    $('#tblTable').dataTable({
+        data: dsTable,
+        columns: dsHead
+    });
+}
+
+//Some fields have characters that don't allow for proper formatting, this is a running list of things that shouldn't be in the data.
+function cleanData(str) {
+    return str.replace(/_/g, " ").replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); });
 }
